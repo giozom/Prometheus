670bd6adf09f7128d831843a8279ecfe5289176f
Michael Bain <mike@thealphatester.com>
2011-01-18 12:49:23 +0800

    initial commit, just got WatiN working in an NUnit test

---
+++ Prometheus.Example/TestStuff.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using WatiN.Core;
+
+namespace Prometheus.Example
+{
+    [TestFixture]
+    public class TestStuff
+    {
+        [Test]
+        public void DoSomethingWithGoogle()
+        {
+            Browser ie = new IE();
+            ie.GoTo("http://www.google.com.au");
+            ie.TextField(Find.ByName("q")).TypeText("hello");
+            ie.Button(Find.ByName("btnG")).Click();
+
+            Assert.That(ie.Title, Is.EqualTo("hello - Google Search")); 
+            ie.Close();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,2 @@
+You will need to install specflow (http://www.specflow.org)
+
