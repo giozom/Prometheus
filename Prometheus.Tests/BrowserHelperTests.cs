9cfbf530c4b4cde5a35eccafd748bb9c92f76382
Michael Bain <mike@thealphatester.com>
2011-01-21 00:46:06 +1100

    Added proto BrowserHelper and first units tests

---
+++ Prometheus.Tests/BrowserHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace Prometheus.Tests
+{
+    [TestFixture]
+    public class BrowserHelperTests
+    {
+
+        [Test]
+        public void ShouldSetTheBrowser()
+        {
+            var mockBrowser = new Mock<Browser>().Object;
+            BrowserHelper.Browser = mockBrowser;
+            Assert.That(BrowserHelper.Browser, Is.EqualTo(mockBrowser));
+        }
+
+        [Test]
+        public void ShouldKeepTheExistingBrowserIfOneExistsWhenStartCalled()
+        {
+            var mockBrowser = new Mock<Browser>().Object;
+            BrowserHelper.Browser = mockBrowser;
+            BrowserHelper.Start();
+            Assert.That(BrowserHelper.Browser, Is.EqualTo(mockBrowser));   
+        }
+
+        [Test]
+        public void ShouldBringTheBrowserToFrontWhenStarted()
+        {
+            var mockBrowser = new Mock<Browser>();
+            BrowserHelper.Browser = mockBrowser.Object;
+            BrowserHelper.Start();
+            mockBrowser.Verify(browser => browser.BringToFront(), Times.Once());
+  
+        }
+
+    }
+}
---
+++ Prometheus/BrowserHelper.cs
@@ -0,0 +1,18 @@
+﻿using WatiN.Core;
+
+namespace Prometheus
+{
+    public static class BrowserHelper
+    {
+        public static Browser Browser { get; set; } 
+ 
+        public static void Start()
+        {
+            if (Browser == null)
+            {
+                Browser = new IE();
+            }
+            Browser.BringToFront();
+        }
+    }
+}
