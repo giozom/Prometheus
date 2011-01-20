ee1780b279cad0b6df25722b6425256d8e658a38
Michael Bain <mike@thealphatester.com>
2011-01-20 08:34:02 +0800

    Move opening and closing the browser to setup and tear down attributes

---
+++ Prometheus.Example/Features/TestSetupAndTearDown.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using WatiN.Core;
+
+namespace Prometheus.Example.Features
+{
+    [SetUpFixture]
+    class TestSetupAndTearDown
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            NastyStaticThing.Ie = new IE();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            NastyStaticThing.Close();
+        }
+    }
+}
--- Prometheus/NastyStaticThing.cs
+++ Prometheus/NastyStaticThing.cs
@@ -5,5 +5,10 @@ namespace Prometheus
     public static class NastyStaticThing
     {
         public static Browser Ie {get; set;}
+
+        public static void Close()
+        {
+            Ie.Close();
+        }
     }
 }
